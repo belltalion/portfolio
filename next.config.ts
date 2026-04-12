c1e98f9f1af99a4ec6e603c478c1f3aa8ec6bf7a
belltalion <xogus4026@gmail.com>
2026-04-12 13:37:03 +0900

    init

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  experimental: {
+    optimizePackageImports: ['@devup-ui/reset-css'],
+  },
+  reactCompiler: true,
+}
+
+export default nextConfig;
